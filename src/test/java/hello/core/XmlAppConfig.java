f5ec6d105056cc467836ca43004639747d75e3e6
jaeeunjeong <jejeong000@gmail.com>
2021-11-29 22:18:45 +0900

    Test : 다양한 설정 형식 지원 - 자바 코드, XML
    스프링의 ApplicationContext는 BeanFactory를 상속받는데, 두 인터페이스의 목적은 객체를 관리하는 것에 있다.
    이 둘을 AnnotationConfigApplicationContext이나 GenericXmlApplicationContext등이 상속받아 사용하기에 bean 설정을 기호에 맞게(java코드 형식이나 xml 형식과 같은) 해줄 수 있다.

---
+++ src/test/java/hello/core/XmlAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+/**
+ * 스프링의 ApplicationContext는 BeanFactory를 상속받는데, 두 인터페이스의 목적은 객체를 관리하는 것에 있다.
+ * 이 둘을 AnnotationConfigApplicationContext이나 GenericXmlApplicationContext등이 상속받아 사용하기에
+ * bean 설정을 기호에 맞게(java코드 형식이나 xml 형식과 같은) 해줄 수 있다.
+ **/
+public class XmlAppConfig {
+    @Test
+    @DisplayName("XML을 이용하여 설정을 해본다.")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
