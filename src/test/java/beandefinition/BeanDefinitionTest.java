bd1f69ed05693eb978a8a1d5a3b484d4f6c50dc7
jaeeunjeong <jejeong000@gmail.com>
2021-11-29 23:15:38 +0900

    Test : 스프링 빈 설정 메타 정보 - BeanDefinition 대해 이해하기 위한 테스트 BeanDefinition을 통해 다양한 형태로 설정할 수 있다.

---
+++ src/test/java/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * Annotation기반, xml 기반을 확인한다.
+ * annotation기반은 팩토리패턴을 이용한다.
+ **/
+public class BeanDefinitionTest {
+    //    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName" + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
