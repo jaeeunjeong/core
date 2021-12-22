76f650b7779cd35b477de5c1d5c4ca1a16260ba7
jaeeunjeong <jejeong000@gmail.com>
2021-12-22 18:28:15 +0900

    Add : @PostConstruct, @PreDestroy 애노테이션을 사용
    변경해야할 코드가 적다.
    java에서 제공하는 어노테이션이기에 (표준) 범용성이 높다.
    단, 외부 라이브러리에서는 적용불가하기에 외부 라이브러리를 초기화/ 종료할 때는 @Bean의 설정 정보를 이용하자.
    
    결과
    connect = null
    call : null message : 초기화 연결 메시지
    NetworkClient.PostConstruct
    connect = https://hello-core.dev
    call : https://hello-core.dev message : 초기화 연결 메시지
    18:25:26.549 [main] DEBUG org.springframework.context.annotation.AnnotationConfigApplicationContext - Closing org.springframework.context.annotation.AnnotationConfigApplicationContext@6aaa5eb0, started on Wed Dec 22 18:25:25 KST 2021
    NetworkClient.PreDestroy
    close : https://hello-core.dev

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * 빈 생명주기를 알아야하는 이유.
  * -> 네트워크나 DB연결과 같이 다른 애플리케이션과의 연결이 필요할 때, 객체를 언제 연결 시켜줄 것인가?
@@ -10,14 +13,15 @@ import org.springframework.beans.factory.InitializingBean;
  * 2. 의존성을 주입된 객체를 사용할 떄-> 이미 늦음.
  * 정답 : 의존성을 주입한 후, 사용 전에 초기화해준다.
  * 그렇다면 의존성 주입 시기를 어떻게 알 수 있을까? -> 스프링과 자바에서 제공하는 빈 생명주기 콜백을 이용한다.
- * <p>
+ *
  * 이러한 작업은 꼭 종료까지 해줘야한다.
- * <p>
+ *
  * cf) 초기화 콜백 : 빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출
  * 소멸전 콜백 : 빈이 소멸되기 직전에 호출
- * <p>
+ *
  * -> 1. 인터페이스 InitializingBean, DisposableBean를 이용.
- * -> 2. 어노테이션을 이용하여 빈 등록 초기화, 소멸 메서드 지정
+ * -> 2. 설정정보에서 빈 등록 초기화, 소멸 메서드 지정
+ * -> 3. 어노테이션을 이용하여 초기화와 종료를 실행
  */
 public class NetworkClient{
 
@@ -44,13 +48,15 @@ public class NetworkClient{
         System.out.println("close : " + url);
     }
 
+    @PreDestroy
     public void close() throws Exception {
-        System.out.println("NetworkClient.close");
+        System.out.println("NetworkClient.PreDestroy");
         disconnect();
     }
 
+    @PostConstruct
     public void init() throws Exception {
-        System.out.println("NetworkClient.init");
+        System.out.println("NetworkClient.PostConstruct");
         connect();
         call("초기화 연결 메시지");
     }
