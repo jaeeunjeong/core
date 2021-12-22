398b40ad669660ff000e1addd966b1971d542129
jaeeunjeong <jejeong000@gmail.com>
2021-12-22 18:23:37 +0900

    Add : 설정정보에 빈 등록 초기화, 소멸 메서드 지정
    메서드 이름을 자유롭게 줄 수 있다.
    설정 정보를 사용하기에 외부 라이브러리에서도 사용 가능하다.
    추가로 @Bean의 destroyMethod의 default가 (inferred)로 되어있는데, close나 shutdown이름을 자동으로 호출한다.
    
    결과
    connect = null
    call : null message : 초기화 연결 메시지
    NetworkClient.init
    connect = https://hello-core.dev
    call : https://hello-core.dev message : 초기화 연결 메시지
    18:19:49.486 [main] DEBUG org.springframework.context.annotation.AnnotationConfigApplicationContext - Closing org.springframework.context.annotation.AnnotationConfigApplicationContext@3498ed, started on Wed Dec 22 18:19:47 KST 2021
    NetworkClient.close
    close : https://hello-core.dev

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -17,8 +17,9 @@ import org.springframework.beans.factory.InitializingBean;
  * 소멸전 콜백 : 빈이 소멸되기 직전에 호출
  * <p>
  * -> 1. 인터페이스 InitializingBean, DisposableBean를 이용.
+ * -> 2. 어노테이션을 이용하여 빈 등록 초기화, 소멸 메서드 지정
  */
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -43,15 +44,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
