46df3536b11211b880272370841c35c7ee4341d5
jaeeunjeong <jejeong000@gmail.com>
2021-12-31 21:11:01 +0900

    Add : 웹 스코프 -request 스코프 예제 만들기 프록시 방식을 이용해서 설정 @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)

---
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -0,0 +1,17 @@
+package hello.core.logdemo;
+
+import hello.core.common.MyLogger;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public LogDemoService(MyLogger myLogger) {
+        this.myLogger = myLogger;
+    }
+
+    public void logic(String id) {
+        myLogger.log("service id : " + id);
+    }
+}
